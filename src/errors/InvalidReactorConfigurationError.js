6e391827d6eec4e8e7970f55a9ad1946082138f7
Adam Letizia <adam@basistheory.com>
2021-08-31 08:20:00 -0500

    fix(eng-1265): Add support for object format of configuration errors

--- src/errors/InvalidReactorConfigurationError.js
+++ src/errors/InvalidReactorConfigurationError.js
@@ -1,10 +1,18 @@
 const BasisTheoryReactorError = require('./BasisTheoryReactorError');
 
 class InvalidReactorConfigurationError extends BasisTheoryReactorError {
-  constructor(propertyName, propertyError) {
-    super('Invalid Reactor Configuration', 400, undefined, {
-      [propertyName]: [propertyError],
-    });
+  constructor(errors) {
+    const validationErrors = {};
+    if (typeof errors === 'object') {
+      for (const property in errors) {
+        validationErrors[property] = Array.isArray(errors[property])
+          ? errors[property].map((e) => e.toString())
+          : [errors[property].toString()];
+      }
+    } else if (typeof errors === 'string') {
+      validationErrors[errors] = [`${errors} is invalid`];
+    }
+    super('Invalid Reactor Configuration', 400, undefined, validationErrors);
   }
 }
 
